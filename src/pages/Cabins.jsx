8930dcc6b6acfa652a64cc79cb1a54fbbb56273c
laura bristot <laurabristotbarbi@hotmail.com>
2024-01-03 13:47:53 -0300

    8 - Connecting Supabase With Our React App

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,10 +1,19 @@
+import { useEffect } from 'react'
+import { getCabins } from '../services/apiCabins'
 import { Heading, Row } from '../ui'
 
 function Cabins() {
+  useEffect(() => {
+    getCabins().then((data) => console.log(data))
+  }, [])
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
       <p>TEST</p>
+      <img
+        src="https://gncuupjmfadawyccyljg.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg"
+        alt=""
+      />
     </Row>
   )
 }
