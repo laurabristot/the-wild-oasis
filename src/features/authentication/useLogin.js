67a855fad5c7f778de6326e2ae6bcd762f135dc3
laura bristot <laurabristotbarbi@hotmail.com>
2024-01-08 19:52:13 -0300

    43 - Fixing an Important Bug

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -10,7 +10,7 @@ export default function useLogin() {
   const { isLoading, mutate: login } = useMutation({
     mutationFn: ({ email, password }) => loginAPI({ email, password }),
     onSuccess: (user) => {
-      queryClient.setQueriesData(['user'], user)
+      queryClient.setQueryData(['user'], user.user)
       navigate('/dashboard', { replace: true })
     },
     onError: () => {
